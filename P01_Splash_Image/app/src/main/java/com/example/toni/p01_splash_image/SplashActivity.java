b4149f9f7a1e3beadb25d4cb1c8943313440e37c
Toni Lázaro <anlazes@gmail.com>
2015-10-21 01:37:42 +0200

    Añadido un gif a Splash Screen

--- P01_Splash_Image/app/src/main/java/com/example/toni/p01_splash_image/SplashActivity.java
+++ P01_Splash_Image/app/src/main/java/com/example/toni/p01_splash_image/SplashActivity.java
@@ -4,18 +4,27 @@ package com.example.toni.p01_splash_image;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ImageView;
+
+import com.koushikdutta.ion.Ion;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity extends Activity {
 
-    private long SPLASH_DELAY = 3000; //3 segundos
+    //se establece el tiempo para el retardo en 3 segundos (3000 ms)
+    private long SPLASH_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //uso de la libreria Ion para cargar un gif en la Splash Screen
+        ImageView image = (ImageView)findViewById(R.id.imageView2);
+        Ion.with(image).load("http://www.europcar.rs/images/loading.gif");
 
         TimerTask task = new TimerTask() {
             @Override
